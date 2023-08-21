78f4d856401c91cbab9f3a3cad4f5990657c2f00
Massimo Biagioli <biagiolimassimo@gmail.com>
2023-08-21 15:53:43 +0200

    Fix auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "nextjsamplifydemo0b6ee0b2": {
+    "nextjsamplifydemoccaab9e1": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from "@/components/navbar.components";
+import awsExports from "@/aws-exports";
+import {Amplify} from "aws-amplify";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +11,8 @@ export const metadata: Metadata = {
   title: 'NextJs Amplify Demo',
   description: 'NextJs Amplify Demo Project',
 }
+
+Amplify.configure({...awsExports, ssr: true});
 
 export default function RootLayout({
   children,
