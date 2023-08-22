a13b9845a3fb5e904829c30df01a8d4bbd01927f
Massimo Biagioli <biagiolimassimo@gmail.com>
2023-08-22 10:12:22 +0200

    Add Authenticator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+
+import {Authenticator} from "@aws-amplify/ui-react";
+import awsExports from "@/aws-exports";
+import {Amplify, Auth} from "aws-amplify";
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({ ...awsExports, ssr: true });
+Auth.configure({ ...awsExports, ssr: true });
+
 export default function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user?.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
